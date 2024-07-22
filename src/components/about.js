2b516835fe471f5e5762a9f13dbdc6f4b42a28ee
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-07-22 20:38:16 +0100

    added some styling and content to header and about page

--- src/components/about.js
+++ src/components/about.js
@@ -7,27 +7,188 @@ function About() {
       id="about"
       className="d-flex justify-content-center align-items-center"
     >
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
+      {/* <div className="block">
+        <div className="columns is-multiline is-mobile">
+          <div class="column is-one-third">
             <h2 className="title is-4">Matthew g. Da Silva</h2>
           </div>
-          <div className="col-6">
+          <div class="column is-two-thirds">
             <h2 className="subtitle is-4">| Creative Frontend Developer</h2>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-4">
-            <div className="box">
+        <div className="columns">
+          <div class="column is-one-third">
+            <figure class="image is-128x128">
+              <img
+                class="is-rounded"
+                src="https://bulma.io/assets/images/placeholders/128x128.png"
+              />
+            </figure>
+          </div>
+          <div class="column is-two-thirds">
+          <div className="box">
               <p className="text-start">
                 I am an aspiring frontend developer with a passion for
                 responsive design and user experience.
               </p>
             </div>
           </div>
-          <div className="col-md-8 text-center align-items-center 
-                        justify-content-center mx-auto">
-            
+        </div>
+      </div> */}
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <h2 className="title is-4">Matthew g. Da Silva</h2>
+          </div>
+          <div className="col-6">
+            <h2 className="subtitle is-4">Creative Frontend Developer</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="has-text-centered">
+              <figure class="image is-128x128 is-inline-block-mobile">
+                <img
+                  class="is-rounded"
+                  src="https://bulma.io/assets/images/placeholders/128x128.png"
+                />
+              </figure>
+            </div>
+          </div>
+          <div
+            className="col-md-6 text-center align-items-center 
+                        justify-content-center mx-auto"
+          >
+            <p className="text-start">
+              I am an aspiring frontend developer with a passion for responsive
+              design and user experience.
+            </p>
+          </div>
+        </div>
+        <div className="row pt-5">
+          <div className="col-md-4">
+            <h2 className="subtitle is-5">Currently Working with:</h2>
+            <div className="box has-background-info-light has-text-primary-dark">
+              <div className="grid is-col-min-3">
+                <div className="cell">
+                  <button class="button is-small is-responsive is-info is-outlined">
+                    HTML
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-info is-outlined">
+                    SCSS
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-info is-outlined">
+                    Javascript
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-info is-outlined">
+                    React Js
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-info is-outlined">
+                    Bootsrap
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-info is-outlined">
+                    Bulma
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <h2 className="subtitle is-5">Currently Studying:</h2>
+            <div className="box has-background-warning-light has-text-primary-dark">
+              <div className="grid  is-col-min-3">
+                <div className="cell">
+                  <button class="button is-small is-responsive is-warning is-outlined">
+                    Flask
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-warning is-outlined">
+                    Django
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-warning is-outlined">
+                    Noje.js
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-warning is-outlined">
+                    Jquery
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-warning is-outlined">
+                    Responsive Web Design
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <h2 className="subtitle is-5">Experienced in:</h2>
+            <div className="box has-background-success-light has-text-primary-dark">
+              <div className="grid  is-col-min-3">
+                <div className="cell">
+                  <button class="button is-small is-responsive is-success is-outlined">
+                    App Dev.
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-success is-outlined">
+                    Flutter
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-success is-outlined">
+                    Dart
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-success is-outlined">
+                    Javascript
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-success is-outlined">
+                    Java
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-success is-outlined">
+                    SQL
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-success is-outlined">
+                    mySQL
+                  </button>
+                </div>
+                <div className="cell">
+                  <button class="button is-small is-responsive is-success is-outlined">
+                    Firebase
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-5">
+          <div
+            className="col  text-center align-items-center 
+                        justify-content-center"
+          >
+            <button class="button is-light">My Canon Events</button>
           </div>
         </div>
       </div>
--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,29 @@ import React from "react";
 function Header() {
   return (
     <section className="quote-section d-flex justify-content-center align-items-center vh-100">
-      <div className="box text-center">
-        <h1 className="title is-1 text-white"><em>Simplicity</em> is the <br/><span className="title-span">Ultimate</span> <strong>Sophistaction</strong></h1>
-        <p className="subtitle is-6 mt-5">- Leonerdo Da Vinci</p>
+      <div className="orange-box"></div>
+      <div className="row">
+        <div className="col-12">
+          <div className="quote-box box text-center">
+            <h1 id="quote-box-quote-text" className="title is-1">
+              <em>Simplicity</em> is the <br />
+              <span className="title-span">Ultimate</span>{" "}
+              <strong>Sophistaction</strong>
+            </h1>
+            <p className="subtitle is-6 mt-5">- Leonerdo Da Vinci</p>
+          </div>
+        </div>
+        <div
+          className="col-12 text-center align-items-center 
+                        justify-content-center mt-5"
+        >
+          <a
+            href="#about"
+            class="button is-black has-text-white-ter is-fullwidth"
+          >
+            View Portfolio
+          </a>
+        </div>
       </div>
     </section>
   );
